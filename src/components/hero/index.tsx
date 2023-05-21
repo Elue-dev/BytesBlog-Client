78434e249d3fed87ba18aa2d077c3cf8237aa6b8
Wisdom <eluewisdom@gmail.com>
2023-05-21 02:35:42 +0100

    patch: modified hero main text styles for better visibility

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <section className={`h-[70vh] ${styles.hero} mt-2`}>
       <div className={`${styles.content} pt-16`}>
-        <h2 className="leading-12 max-w-md text-4xl">
+        <h2 className="leading-12 text-4xl">
           Unleash your thoughts and pen them down
         </h2>
         <p className="mt-4 font-semibold text-lightTextColor sm:font-normal">
