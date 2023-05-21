10fef340c202cd2a904fcc89092afd5c5c66b2f5
Wisdom <eluewisdom@gmail.com>
2023-05-21 21:30:01 +0100

    feat: styling for 404 not found pages

--- src/components/not_found/index.tsx
+++ src/components/not_found/index.tsx
@@ -1,3 +1,34 @@
+import Button from "../button";
+import bytesLogo from "@/assets/bytesLogo.svg";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function NotFound() {
-  return <div className="text-center text-4xl">404. Page Not Found</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="h-[100vh] bg-primaryColorLight text-center">
+      <div className="sm:p-auto absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 transform p-2 sm:w-auto">
+        <Link to="/" className="flex items-center justify-center">
+          <img src={bytesLogo} alt="BytesBlog Logo" className="h-14" />
+        </Link>
+
+        <p className="mt-2 text-2xl font-normal text-lightGray sm:text-3xl">
+          The page you are requesting for is not available
+        </p>
+        <div className="flex flex-col gap-4 pt-10 sm:flex-row">
+          <Button
+            className="w-full border-2 border-primaryColor bg-primaryColor p-3 text-2xl font-normal uppercase text-white sm:w-1/2"
+            onClick={() => navigate("/")}
+          >
+            Go Home
+          </Button>
+          <Button
+            className="w-full border-2 border-primaryColor bg-primaryColorLight p-3 text-2xl font-normal uppercase text-primaryColor sm:w-1/2"
+            onClick={() => navigate("/create-account")}
+          >
+            Sign Up
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
